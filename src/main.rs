a157b076dc66d8a67df460cb16ea28d060534e81
Dmitry Shuranov <dvshur@gmail.com>
2020-06-10 18:58:31 +0300

    works for trivial case: (1, 1), secret in subgroup

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,36 @@
 mod polynom;
 mod pow;
-mod secret;
 
-use curve25519_dalek::scalar::Scalar;
+use curve25519_dalek::{
+    constants,
+    edwards::{CompressedEdwardsY, EdwardsPoint},
+    scalar::Scalar,
+};
+// use curve25519_dalek::
+// use digest::Digest;
+use digest::{Digest, Reset};
+use ed25519_dalek::{ExpandedSecretKey, Keypair, PublicKey, SecretKey, Sha512, Signature};
 use polynom::Polynom;
-use secret::Secret;
 use std::convert::TryInto;
 
 // Secret sharing parameters
 // Parties number
 const N: usize = 5;
 // Threshold
-const T: usize = 3;
+const T: usize = 1;
 
+#[allow(non_snake_case)]
 fn main() {
-    // secret to share
-    let secret_string = "Hello, world!";
+    // key pair to split
+    let mut csprng = rand::thread_rng();
+
+    let rnd_scalar = Scalar::random(&mut csprng);
+    let secret_key = SecretKey::from_bytes(rnd_scalar.as_bytes()).unwrap();
+    let public_key = PublicKey::from(&secret_key);
+    let key_pair = Keypair {
+        secret: secret_key,
+        public: public_key,
+    };
 
     // Parties indexes
     let xs: [Scalar; N] = [
@@ -26,53 +41,113 @@ fn main() {
         Scalar::from(5u8),
     ];
 
-    let secret = Scalar::from_bytes_mod_order(to_canonical_bytes(secret_string).unwrap());
+    let secret = Scalar::from_bytes_mod_order(key_pair.secret.to_bytes());
 
-    // SHARE
+    assert!(secret.as_bytes() == key_pair.secret.as_bytes());
+
+    // KEY SHARES
     let shares = shamir_share(&xs, &secret);
 
-    // RESHARE
-    // 1. calculate sij
-    // i — sender from group 0..T
-    // j — recipient from group -0..N'
-    // for now consider simple case:
-    // T' = T and N' = N
-    let senders: &[Scalar; T] = &xs[0..T].try_into().expect("fatal error");
-    let senders_shares: &[Scalar; T] = &shares[0..T].try_into().expect("fatal error");
-
-    let mut ss = [[Scalar::zero(); N]; T];
-    for i in 0..T {
-        // each sender chares his secret with Shamir SS
-        // and sends them to all recipients
-        ss[i] = shamir_share(&xs, &senders_shares[i]);
+    for share in &shares {
+        assert!(*share == secret);
+        assert!(share.as_bytes() == key_pair.secret.as_bytes());
     }
 
-    // 2. reconstruct each party from shares sent to it
-    let shares_v2 = [
-        shamir_reconstruct(&senders, &[ss[0][0], ss[1][0], ss[2][0]]),
-        shamir_reconstruct(&senders, &[ss[0][1], ss[1][1], ss[2][1]]),
-        shamir_reconstruct(&senders, &[ss[0][2], ss[1][2], ss[2][2]]),
-        shamir_reconstruct(&senders, &[ss[0][3], ss[1][3], ss[2][3]]),
-        shamir_reconstruct(&senders, &[ss[0][4], ss[1][4], ss[2][4]]),
-    ];
+    // SIGN
+    let message: &[u8] = b"Hello, world";
 
-    // RECONSTRUCT
-    let reconstruct_participants: &[Scalar; T] = &xs[0..T].try_into().expect("fatal error");
-    let reconstruct_participants_shares: &[Scalar; T] =
-        &shares_v2[0..T].try_into().expect("fatal error");
+    // T signers, [0..T]
+    let signers: &[Scalar; T] = &xs[0..T].try_into().expect("fatal error");
+    let signers_shares: &[Scalar; T] = &shares[0..T].try_into().expect("fatal error");
 
-    // check that shares are not equal
-    for i in 0..N {
-        assert!(shares[i] != shares_v2[i]);
+    assert!(signers.len() == 1);
+
+    // make expanded secret keys out of shares
+    let exp_secret_keys: [(Scalar, [u8; 32]); T] = {
+        let mut res = [(Scalar::zero(), [0u8; 32]); T];
+        for i in 0..T {
+            // todo should I make it out of share, or apply lagrange coeff first?
+            res[i] = expanded_secret_key(&signers_shares[i].as_bytes());
+        }
+        res
+    };
+
+    assert!(exp_secret_keys.len() == 1);
+    assert!(exp_secret_keys[0].0 != shares[0]);
+
+    // generate ri
+    let rs: [Scalar; T] = {
+        let mut res = [Scalar::zero(); T];
+        for i in 0..T {
+            let mut h = Sha512::new();
+
+            h.input(&exp_secret_keys[i].1);
+            h.input(&message);
+
+            res[i] = Scalar::from_hash(h.clone());
+        }
+        res
+    };
+
+    // calculate Ri
+    let Rs: [EdwardsPoint; T] = {
+        let mut res = [EdwardsPoint::default(); T];
+        for i in 0..T {
+            res[i] = &rs[i] * &constants::ED25519_BASEPOINT_TABLE;
+        }
+        res
+    };
+
+    // sum Ri to get R
+    let R: CompressedEdwardsY = {
+        let R: EdwardsPoint = Rs.iter().sum();
+        R.compress()
+    };
+
+    assert!(Rs[0].compress() == R);
+
+    // calculate k
+    let k = {
+        let mut h = Sha512::new();
+        h.input(R.as_bytes());
+        h.input(key_pair.public.as_bytes());
+        h.input(&message);
+        Scalar::from_hash(h)
+    };
+
+    // lagrange coefficients
+    let lagrange_coeffs = lagrange_coeffs_at_zero(signers);
+
+    assert!(lagrange_coeffs[0] == Scalar::one());
+
+    let ss: [Scalar; T] = {
+        let mut res = [Scalar::zero(); T];
+        for i in 0..T {
+            res[i] = &(&k * &exp_secret_keys[i].0) + &rs[i]; // * &lagrange_coeffs[i];
+        }
+        res
+    };
+
+    let s: Scalar = ss.iter().sum();
+
+    // println!("r: {:?}, R: {:?}, k: {:?}, s: {:?}", rs[0], Rs[0], k, ss[0]);
+
+    assert!(s == ss[0]);
+
+    let sig_bytes = [R.to_bytes(), s.to_bytes()].concat();
+
+    let signature = Signature::from_bytes(&sig_bytes).unwrap();
+
+    // sign using staightforward way
+    // let signature2 = ExpandedSecretKey::from(&key_pair.secret).sign(message, &key_pair.public);
+
+    // VERIFY
+    match key_pair.public.verify(message, &signature) {
+        Ok(_) => println!("Sig verified, cool"),
+        Err(_) => println!("Failed"),
     }
 
-    let secret_reconstructed =
-        shamir_reconstruct(reconstruct_participants, reconstruct_participants_shares);
-
-    let secret_reconstructed_string =
-        from_canonical_bytes(secret_reconstructed.as_bytes()).unwrap();
-
-    println!("{}", secret_reconstructed_string);
+    // println!("{}", secret_reconstructed_string);
 }
 
 fn lagrange_coeffs_at_zero(xs: &[Scalar; T]) -> [Scalar; T] {
@@ -104,13 +179,32 @@ fn shamir_share(xs: &[Scalar; N], secret: &Scalar) -> [Scalar; N] {
     res
 }
 
-fn shamir_reconstruct(xs: &[Scalar; T], shares: &[Scalar; T]) -> Scalar {
-    let lagrange_coeffs = lagrange_coeffs_at_zero(xs);
+// fn shamir_reconstruct(xs: &[Scalar; T], shares: &[Scalar; T]) -> Scalar {
+//     let lagrange_coeffs = lagrange_coeffs_at_zero(xs);
 
-    let mut res = Scalar::zero();
-    for i in 0..T {
-        res += lagrange_coeffs[i] * shares[i];
-    }
+//     let mut res = Scalar::zero();
+//     for i in 0..T {
+//         res += lagrange_coeffs[i] * shares[i];
+//     }
 
-    res
+//     res
+// }
+
+fn expanded_secret_key(secret_key: &[u8; 32]) -> (Scalar, [u8; 32]) {
+    let mut h: Sha512 = Sha512::default();
+    let mut hash: [u8; 64] = [0u8; 64];
+    let mut lower: [u8; 32] = [0u8; 32];
+    let mut upper: [u8; 32] = [0u8; 32];
+
+    h.input(secret_key);
+    hash.copy_from_slice(h.result().as_slice());
+
+    lower.copy_from_slice(&hash[00..32]);
+    upper.copy_from_slice(&hash[32..64]);
+
+    lower[0] &= 248;
+    lower[31] &= 63;
+    lower[31] |= 64;
+
+    (Scalar::from_bits(lower), upper)
 }
